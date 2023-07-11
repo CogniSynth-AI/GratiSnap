7c654df33cc6d5bfbe35b0c7598a4203dbcd2314
Sarthak Sarans <63066248+SarthakSarans@users.noreply.github.com>
2023-07-11 14:50:47 -0500

    Update firebase.js
    Hide config for public view

--- firebase.js
+++ firebase.js
@@ -14,19 +14,7 @@ import {createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword} fro
 
 const firebaseConfig = {
 
-  apiKey: "AIzaSyDUlytbK9GH3uvoXqCWstwWbgo4OPxDmPo",
-
-  authDomain: "gratisnap.firebaseapp.com",
-
-  projectId: "gratisnap",
-
-  storageBucket: "gratisnap.appspot.com",
-
-  messagingSenderId: "999571422888",
-
-  appId: "1:999571422888:web:3690dd831032fb1f534c9d",
-
-  measurementId: "G-028CEEEX6T"
+/* Hidden*/
 
 };
 
